c946c306fbf6e44cc82a4658a9856ffc2f6068e6
kycnky <kayacan.kaya@ug.bilkent.edu.tr>
2017-11-04 01:04:04 +0300

    forth trial

--- src/trackIn/src/Intern.java
+++ src/trackIn/src/Intern.java
@@ -1,7 +1,6 @@
 public class Intern {
 
     public Intern(){
-        
-    }
 
+    }
 }
---
+++ src/trackIn/src/User.java
@@ -0,0 +1,7 @@
+public abstract class User {
+
+    private String name;
+    private String eMail;
+    private String phoneNumber;
+
+}
