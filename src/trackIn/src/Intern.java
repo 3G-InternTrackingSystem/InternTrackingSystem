3cc32b7051d3eda2dec4a27f077cf4c3f25f24db
alisemi <aliyenimol@gmail.com>
2017-11-04 01:49:30 +0300

    playing with git

--- src/trackIn/src/Intern.java
+++ src/trackIn/src/Intern.java
@@ -5,4 +5,11 @@ public class Intern {
         this.name = name;
     }
 
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
