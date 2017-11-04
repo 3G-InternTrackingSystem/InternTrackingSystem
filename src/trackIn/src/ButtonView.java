68f8b7a07d5b819824bc1d6f64203a5a38e42813
alisemi <aliyenimol@gmail.com>
2017-11-04 15:56:52 +0300

    Files are not included in the previous commit, now the project has been sent

---
+++ src/trackIn/src/ButtonView.java
@@ -0,0 +1,19 @@
+import javax.faces.application.FacesMessage;
+import javax.faces.bean.ManagedBean;
+import javax.faces.context.FacesContext;
+import javax.faces.event.ActionEvent;
+
+@ManagedBean
+public class ButtonView {
+
+    public void login(ActionEvent actionEvent) {
+        //TODO call the AccountManagerController functions here
+    }
+
+    public void signUp( ActionEvent actionEvent) {
+        //TODO call the AccountManagerController functions here
+    }
+    public void addMessage(String summary) {
+
+    }
+}
---
+++ src/trackIn/src/UserBean.java
@@ -0,0 +1,45 @@
+import javax.faces.bean.ManagedBean;
+
+@ManagedBean
+public class UserBean {
+    private String userName;
+    private String password;
+    private String eMail;
+    private String userType;
+
+    public UserBean(){
+
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String geteMail() {
+        return eMail;
+    }
+
+    public void seteMail(String eMail) {
+        this.eMail = eMail;
+    }
+
+    public String getUserType() {
+        return userType;
+    }
+
+    public void setUserType(String userType) {
+        this.userType = userType;
+    }
+}
